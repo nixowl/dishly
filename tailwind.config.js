390bad5ff6deff97e1ce4de96059adf2b4b6f0e2
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-07 13:29:49 +0100

    add missing file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ import { nextui } from "@nextui-org/react"
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+   "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
