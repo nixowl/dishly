3153d5f63909261def42d11dd9e19aedb086247a
Gabriella Mondvay <gabriellamondvay@gmail.com>
2024-02-07 13:58:00 +0100

    add basic mapping over fetched results for hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,22 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import { Image } from '@nextui-org/react';
+
 function Hero() {
+  const recipes = useSelector((state: RootState) => state.recipes.heroRecipes);
   return (
-    <div>Hero</div>
-  )
+    <article className="w-full bg-red-500">
+      {recipes.map((recipe, index) => (
+        <div key={index}>
+              <Image
+                  removeWrapper
+                  src={recipe.image}
+                  alt={recipe.label}
+              />
+        </div>
+      ))}
+    </article>
+  );
 }
 
-export default Hero
+export default Hero;
